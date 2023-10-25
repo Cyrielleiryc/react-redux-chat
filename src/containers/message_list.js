da5be4ff7c35e30e857348a766624b2cd217c0a6
Cyrielle Marchand <cyrielle.marchand@gmail.com>
2023-10-25 13:00:34 +0200

    added interval for refresh

--- src/containers/message_list.js
+++ src/containers/message_list.js
@@ -11,6 +11,19 @@ class MessageList extends Component {
     this.fetchMessages();
   }
 
+  componentDidMount() {
+    this.refresher = setInterval(this.fetchMessages, 5000);
+  }
+
+  componentDidUpdate() {
+    this.list.scrollTop = this.list.scrollHeight;
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.refresher);
+  }
+
+
   fetchMessages = () => {
     this.props.fetchMessages(this.props.selectedChannel);
   }
@@ -21,10 +34,12 @@ class MessageList extends Component {
         <div className="channel-title">
           <span>Channel #{this.props.selectedChannel}</span>
         </div>
-        <div className="channel-content">
-          { this.props.messages.map((message, index) => (
-            <Message key={index} message={message} />
-          ))}
+        <div className="channel-content" ref={(list) => { this.list = list; }}>
+          {
+            this.props.messages.map((message) => {
+              return <Message key={message.id} message={message} />;
+            })
+          }
         </div>
         <MessageForm />
       </div>
