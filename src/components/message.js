7db35ce8e7b60e82e7faf5436ae8adf8b553d2b4
Cyrielle Marchand <cyrielle.marchand@gmail.com>
2023-10-26 12:44:40 +0200

    coloured the author's name with function

--- src/components/message.js
+++ src/components/message.js
@@ -1,10 +1,23 @@
+const stringToColour = (str) => {
+  let hash = 0;
+  str.split('').forEach(char => {
+    hash = char.charCodeAt(0) + ((hash << 5) - hash)
+  })
+  let colour = '#'
+  for (let i = 0; i < 3; i++) {
+    const value = (hash >> (i * 8)) & 0xff
+    colour += value.toString(16).padStart(2, '0')
+  }
+  return colour
+}
+
 function Message({message}) {
   const time = new Date(message.created_at).toLocaleTimeString();
 
   return (
     <div className="message-container">
       <i className="author">
-        <span>{message.author}</span>
+        <span style={{color: stringToColour(message.author)}}>{message.author}</span>
         <small>{time}</small>
       </i>
       <p>{message.content}</p>
